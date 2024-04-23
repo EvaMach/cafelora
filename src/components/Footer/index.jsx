42c961094d5384218d698d0dc075fa823b4f8528
Eva Machová <eva.machova@newtontech.cz>
2024-04-23 18:24:24 +0200

    split into components

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer__content">
+          Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
